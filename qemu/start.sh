bbdc05d799acd4e59af6ec0d4379064a3ad4e818
Norman Zhi <normanzyb@gmail.com>
2024-07-06 16:15:04 +0100

    Update script to start QEMU

--- qemu/start.sh
+++ qemu/start.sh
@@ -8,21 +8,23 @@ if (($# != 0)); then
 	monitor=stdio
 fi
 
-#qemu-img create -f qcow2 -F qcow2 -b nand_dump_base_oob256.qcow2 nand_overlay.qcow2
+# Create overlay qcow2 files as snapshot
 qemu-img create -f qcow2 -F qcow2 -b nand_dump_oob_base.qcow2 nand_overlay.qcow2
 qemu-img create -f qcow2 -F qcow2 -b mmc_base.qcow2 mmc_overlay.qcow2
 
+# Start QEMU
 eval $prefix ./qemu-system-mipsel \
 	-M noah_np1380 -cpu JZ4740 \
-	-s \
 	-d guest_errors,unimp \
 	-bios bootrom.bin \
 	-parallel null \
 	-serial tcp:localhost:7645 \
 	-monitor $monitor \
-	-blockdev driver=file,node-name=nand,filename=nand_overlay.qcow2 \
-	-blockdev qcow2,node-name=nand,file=nand \
-	-blockdev driver=file,node-name=mmc,filename=mmc_overlay.qcow2 \
+	-blockdev driver=file,node-name=nand_qcow2,filename=nand_overlay.qcow2 \
+	-blockdev qcow2,node-name=nand,file=nand_qcow2 \
 	-global ingenic-emc-nand.drive=nand \
+	-blockdev driver=file,node-name=mmc_qcow2,filename=mmc_overlay.qcow2 \
+	-blockdev qcow2,node-name=mmc,file=mmc_qcow2 \
+	-device sd-card,spec_version=3,drive=mmc \
 	-display vnc=:10 \
 	"$@"
